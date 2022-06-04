02264770fcafc58c19c70dc157c2640ab4ba26ae
Chau Tran <nartc7789@gmail.com>
2022-06-03 23:47:41 -0500

    setup nswag

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  apiUrl: "https://api.realworld.io",
 };
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { enableProdMode } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
+import { API_BASE_URL } from "./app/shared/api";
 import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [{ provide: API_BASE_URL, useValue: environment.apiUrl }],
+}).catch((err) => console.error(err));
