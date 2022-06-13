d7d6794700943d359aeb9a4bc9675354ed4f1f95
Chau Tran <nartc7789@gmail.com>
2022-06-13 11:45:49 -0500

    test: app component with testing library

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,37 +1,39 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { RouterTestingModule } from '@angular/router/testing';
+import { render } from '@testing-library/angular';
 import { App } from './app.component';
 import { AuthStore } from './shared/data-access/auth.store';
 
 describe(App.name, () => {
-  let fixture: ComponentFixture<App>;
   let mockedAuthStore: jasmine.SpyObj<AuthStore>;
+
+  async function setupAppRender() {
+    return await render(App, {
+      providers: [{ provide: AuthStore, useValue: mockedAuthStore }],
+    });
+  }
 
   beforeEach(() => {
     mockedAuthStore = jasmine.createSpyObj(AuthStore.name, ['init']);
-
-    fixture = TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule],
-      providers: [{ provide: AuthStore, useValue: mockedAuthStore }],
-    }).createComponent(App);
   });
 
-  it('should create component', () => {
-    const component = fixture.componentInstance;
+  it('Then create component', async () => {
+    const rendered = await setupAppRender();
+
+    const component = rendered.fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 
-  it('should call authStore.init on component init', () => {
-    fixture.detectChanges();
-    expect(mockedAuthStore.init).toHaveBeenCalled();
+  describe('When component init', () => {
+    it('Then call authStore.init', async () => {
+      const rendered = await setupAppRender();
+      expect(mockedAuthStore.init).toHaveBeenCalled();
+    });
   });
 
-  describe('render', () => {
-    it('should render router-outlet', () => {
-      fixture.detectChanges();
-      const routerOutletElement = fixture.debugElement.query(
+  describe('When render', () => {
+    it('Then render router-outlet', async () => {
+      const rendered = await setupAppRender();
+      const routerOutletElement = rendered.fixture.debugElement.query(
         By.css('router-outlet')
       );
       expect(routerOutletElement).toBeTruthy();
