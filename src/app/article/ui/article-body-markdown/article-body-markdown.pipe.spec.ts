668ec2a7fe2de921191e59c9bb7ab72bc4f8995c
Chau Tran <nartc7789@gmail.com>
2022-06-15 08:38:39 -0500

    test: article body markdown

---
+++ src/app/article/ui/article-body-markdown/article-body-markdown.pipe.spec.ts
@@ -0,0 +1,67 @@
+import { Component, Input } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { render } from '@testing-library/angular';
+import { ArticleBodyMarkdown } from './article-body-markdown.pipe';
+
+@Component({
+  selector: 'test-dummy',
+  template: `
+    <div class="content" [innerHTML]="content | markdown"></div>
+  `,
+  imports: [ArticleBodyMarkdown],
+  standalone: true,
+})
+class TestDummy {
+  @Input() content = '';
+}
+
+describe(ArticleBodyMarkdown.name, () => {
+  async function setupRender(content: string) {
+    return await render(TestDummy, {
+      componentProperties: { content },
+    });
+  }
+
+  describe('Given empty content', () => {
+    it('Then render empty innerHTML', async () => {
+      const { debugElement } = await setupRender('');
+      const contentDiv = debugElement.query(By.css('.content'));
+      expect(contentDiv.nativeElement).toHaveTextContent('');
+    });
+  });
+
+  describe('Given valid markdown content', () => {
+    it('Then render valid html', async () => {
+      const { debugElement } = await setupRender(`**bold** and _italic_`);
+      const contentDiv = debugElement.query(By.css('.content'));
+
+      const transformedParagraph = contentDiv.query(By.css('p'));
+      expect(transformedParagraph).toBeTruthy();
+
+      const boldElement = transformedParagraph.query(By.css('strong'));
+      const italicElement = transformedParagraph.query(By.css('em'));
+
+      expect(boldElement.nativeElement).toHaveTextContent('bold');
+      expect(italicElement.nativeElement).toHaveTextContent('italic');
+    });
+  });
+
+  describe('Given malicious content', () => {
+    it('Then render with sanitized content', async () => {
+      const { debugElement } = await setupRender(
+        `**this is malicious** <script>alert("malicious")</script>`
+      );
+
+      const contentDiv = debugElement.query(By.css('.content'));
+
+      const transformedParagraph = contentDiv.query(By.css('p'));
+      expect(transformedParagraph).toBeTruthy();
+
+      const boldElement = transformedParagraph.query(By.css('strong'));
+      expect(boldElement.nativeElement).toHaveTextContent('this is malicious');
+
+      const scriptElement = transformedParagraph.query(By.css('script'));
+      expect(scriptElement).toBeFalsy();
+    });
+  });
+});
