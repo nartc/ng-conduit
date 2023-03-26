0f848cdb5ca11e4a6c272b5489b4c77a96f15bda
Chau Tran <chautran@Chaus-MacBook-Pro.local>
2023-03-26 13:43:24 -0500

    chore: setup cypress componenet test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,4 +2,11 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
     e2e: { baseUrl: 'http://localhost:4200' },
+    component: {
+        devServer: {
+            framework: 'angular',
+            bundler: 'webpack',
+        },
+        specPattern: '**/*.cy.ts'
+    },
 });
